16ad14f8787118a40a63aa2357c3e8293d35c315
Christopher Bouey <cabouey@gmail.com>
2023-03-27 13:03:25 -0700

    Finished exercise 1 and removed downloaded files

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
-import requests
+import requests, os, zipfile, io
+
+if not os.path.exists('downloads'):
+    os.mkdir('downloads')
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -10,10 +13,18 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
 
+target_path = 'downloads/'
 
 def main():
-    # your code here
-    pass
+    for uri in download_uris:
+        try:
+
+            response = requests.get(uri, stream=True)
+            zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+            zip_file.extractall(target_path)
+            
+        except:
+            continue
 
 
 if __name__ == '__main__':
